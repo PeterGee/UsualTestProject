226c9a92ebeb5ea5ce30702c4f3609f604696946
Petergee <qipeng.ge@edaibu.net>
2018-09-04 14:07:46 +0800

    Kotlin  实现AIDL

---
+++ app/src/main/java/net/gepergee/usualtestproject/activity/service/ServiceTestActivity.kt
@@ -0,0 +1,81 @@
+package net.gepergee.usualtestproject.activity.service
+
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.IBinder
+import android.os.RemoteException
+import android.util.Log
+import android.view.View
+import android.widget.Button
+
+import net.gepergee.aidlservice.IMyAidlInterface
+import net.gepergee.usualtestproject.R
+
+/**
+ * @author petergee
+ * @date 2018/9/3
+ */
+class ServiceTestActivity : Activity(), View.OnClickListener {
+    private var isBinded: Boolean = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            // MyBinder binder = (MyBinder) service;
+            // binder.printWord();
+            val stub = IMyAidlInterface.Stub.asInterface(service) ?: return
+            try {
+                val s = stub.setName("PeterGee")
+                Log.e("tag", "name set is $s")
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
+
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {}
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_service_test)
+        initView()
+    }
+
+    private fun initView() {
+        val btnStartService = findViewById<Button>(R.id.btn_start_service)
+        btnStartService.setOnClickListener(this)
+        val btnStopService = findViewById<Button>(R.id.btn_stop_service)
+        btnStopService.setOnClickListener(this)
+        val btnBindService = findViewById<Button>(R.id.btn_bind__service)
+        btnBindService.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        val intent = Intent()
+        // intent.setClass(this, MyService.class);
+        when (v.id) {
+            R.id.btn_start_service -> startService(intent)
+            R.id.btn_stop_service -> stopService(intent)
+            R.id.btn_bind__service -> {
+                intent.action = "net.gepergee.aidlservice.AidlService"
+                intent.setPackage("net.gepergee.aidlservice")
+                bindService(intent, connection, Context.BIND_AUTO_CREATE)
+                isBinded = true
+            }
+            else -> {
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (isBinded) {
+            unbindService(connection)
+        }
+    }
+}
